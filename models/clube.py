dbf6339f1a08c2d74460ace5578da813f6047ca7
ThiagoSantos21 <thiagolima-santos@hotmail.com>
2024-04-03 19:11:20 -0300

    criação do projeto

---
+++ models/clube.py
@@ -0,0 +1,9 @@
+class Clube:
+    
+    def __init__(self, cnpj, nome, email, senha):
+        self.cnpj = cnpj
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+    
